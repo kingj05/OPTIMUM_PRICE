96df116d86bebff618d991f81b30c9c71ac8c0ec
kingj05 <jkingtanga@gmail.com>
2024-10-01 00:23:54 +0800

    Add files via upload

--- public/script.js
+++ public/script.js
@@ -63,22 +63,18 @@ async function State() {
       if (data.success) {
         jsonInput.value = '';
         showResult(data.message);
-        showAds();
       } else {
         jsonInput.value = '';
         showResult(data.message);
-        showAds();
       }
     } else {
       jsonInput.value = '';
       showResult('Invalid JSON data. Please check your input.');
-      showAds();
     }
   } catch (parseError) {
     jsonInput.value = '';
     console.error('Error parsing JSON:', parseError);
     showResult('Error parsing JSON. Please check your input.');
-    showAds();
   } finally {
     setTimeout(() => {
       button.style.display = 'block';
